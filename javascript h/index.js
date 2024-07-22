0a2fe9436feb664ca81f7afb3d8993eda85b2bea
Hadiya204 <168410223+Hadiya204@users.noreply.github.com>
2024-07-22 16:00:15 +0500

    Add files via upload

---
+++ javascript h/index.js
@@ -0,0 +1,18 @@
+function add(a,b){
+    return a+b;
+}
+console.log(add(4 ,6));
+
+function add(a,b){
+    return a * Number(b);
+}
+console.log(add(2,3));
+let greet = "hello";
+function greeting(name){
+    console.log(greet+name);
+}
+greeting("hi")
+greeting("alice")
+
+
+
